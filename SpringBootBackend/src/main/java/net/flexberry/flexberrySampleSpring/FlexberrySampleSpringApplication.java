063a32df2b7b37cf0e4566f639ecc4ca92e791bb
AntonIV8 <anton-iv87@yandex.ru>
2022-12-07 13:37:43 +0500

    Java SpringBoot backend. Сгенерирован пустой проект на https://start.spring.io/

---
+++ SpringBootBackend/src/main/java/net/flexberry/flexberrySampleSpring/FlexberrySampleSpringApplication.java
@@ -0,0 +1,13 @@
+package net.flexberry.flexberrySampleSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexberrySampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexberrySampleSpringApplication.class, args);
+	}
+
+}
---
+++ SpringBootBackend/src/test/java/net/flexberry/flexberrySampleSpring/FlexberrySampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.flexberry.flexberrySampleSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexberrySampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
