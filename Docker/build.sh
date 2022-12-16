79f311593caa083a5dc5d07bacc0f4f2ee867bc6
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2022-12-16 13:59:20 +0500

    Add backend to docker

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,1 +1,2 @@
 docker build --no-cache -f Dockerfiles/Dockerfile.Postgres -t springbootsample/postgres ../SQL
+docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/backend ../SpringBootBackend
