c19e5468949b69f57011dfffddf0d64966f63ba1
AntonIV8 <anton-iv87@yandex.ru>
2022-12-22 16:26:14 +0500

    Правка аннотаций

--- SpringBootBackend/src/main/java/net/flexberry/flexberrySampleSpring/model/Customer.java
+++ SpringBootBackend/src/main/java/net/flexberry/flexberrySampleSpring/model/Customer.java
@@ -1,10 +1,6 @@
 package net.flexberry.flexberrySampleSpring.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.envers.Audited;
 
 import java.util.List;
@@ -13,11 +9,8 @@ import java.util.UUID;
 @Entity
 @Audited
 @Table(schema = "public", name = "customer")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "primarykey")
 public class Customer {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "primarykey")
     private UUID primarykey;
 
@@ -27,8 +20,7 @@ public class Customer {
     @Column(name = "age")
     private Integer age;
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY,
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER,
             cascade = CascadeType.ALL)
     private List<Comment> comments;
 
